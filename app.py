482d5780495cbb238c66993e3906bb5a051e7858
StartDiggin <dimensions122@gmail.com>
2022-10-16 21:46:03 -0400

    add a user page, created db, and printed names to page

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+db = SQLAlchemy(app)
+
+
+# users model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.id
 
 
 @app.route("/")
@@ -20,9 +36,24 @@ def logout():
 
 
 # add a user to the db
-@app.route("/addUser", methods=['POST'])
+@app.route("/addUser", methods=['POST', 'GET'])
 def addUser():
-    return render_template('home.html')
+    if request.method == "POST":
+        user_name = request.form['name']
+        user_email = request.form['email']
+        user_password = request.form['password']
+        user = Users(name=user_name, email=user_email, password=user_password)
+
+        # add to db
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/addUser')
+        except:
+            return "there was an error adding the user"
+    else:
+        users = Users.query.order_by(Users.created_at)
+        return render_template('userHP.html', users=users)
 
 
 if __name__ == '__name__':
